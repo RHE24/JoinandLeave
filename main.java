df659273fd853f32a1cb37c00ff220fadbd1431c
tv15dsi <thibaut@thebestdutchgames.be>
2013-11-30 17:17:12 +0100

    Update in the main class!
    I have now updated the class

--- main.java
+++ main.java
@@ -1,13 +1,21 @@
 package me.tv15dsi.JoinandLeave;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
 	
 	public void onEnable() {
-		
+		getLogger().info("JoinandLeave loaded!");
 	}
 	public void onDisable() {
+		getLogger().info("JoinandLeave stopped!");
+	}
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player p = (Player) sender;
 		
+		return false;
 	}
 }
