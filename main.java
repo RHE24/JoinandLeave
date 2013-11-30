b5c8b866a5a20d351c6b980c46e5faff6be81fd3
tv15dsi <thibaut@thebestdutchgames.be>
2013-11-30 17:14:08 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+package me.tv15dsi.JoinandLeave;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	public void onDisable() {
+		
+	}
+}
